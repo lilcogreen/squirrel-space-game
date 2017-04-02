d9c7eaae2a91d8d4ec7992f944a5be5376c3f70b
adotwin <adotwin42@gmail.com>
2017-04-01 22:00:03 -0700

    Made oxygen count down

--- new-template/assets/javascript/module/HUD.js
+++ new-template/assets/javascript/module/HUD.js
@@ -17,29 +17,30 @@ define(function(){
     return{
         init: function(game) {
             _game = game;
+            _oxygen = 100.0;
         },
         create: function() {
 		        _scoreText = _game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#d7adf0' });
-            _oxygenText = _game.add.text(16, 36, 'oxygen: 0', { fontSize: '32px', fill: '#d7adf0' });
-            _fuelText = _game.add.text(16, 56, 'fuel: 0', { fontSize: '32px', fill: '#d7adf0' });
+            _oxygenText = _game.add.text(16, 46, 'oxygen: 100', { fontSize: '32px', fill: '#d7adf0' });
+            _fuelText = _game.add.text(16, 76, 'fuel: 100', { fontSize: '32px', fill: '#d7adf0' });
         },
         addToScore: function(value) {
-            _score+=10;
+            _score+=value;
         },
         updateScoreText: function() {
             _scoreText.content = 'Score: ' + _score;
         },
-        addToOxygen: function(value) {
-            _score+=10;
+        depleteOxygen: function() {
+            _oxygen-=.01;
         },
         updateOxygenText: function() {
-            _oxygenText.content = 'Oxygen: ' + _score;
+            _oxygenText.text = 'Oxygen: ' + Math.round(_oxygen);
         },
         addToFuel: function(value) {
             _score+=10;
         },
         updateFuelText: function() {
             _fuelText.content = 'Fuel: ' + _score;
-          }
+        }
     }
 });
