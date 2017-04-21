d7fb8fb179fc65bf5628600d5a3fedf6d2f5d2f8
adotwin <adotwin42@gmail.com>
2017-04-20 22:49:46 -0700

    Adding new Eevee with movement

--- new-template/assets/javascript/module/Thing.js
+++ new-template/assets/javascript/module/Thing.js
@@ -14,16 +14,16 @@ define(['lib/phaser.min', 'module/Player'],function(Phaser, Player){
             _game = game;
           },
         preload: function() {
-              _game.load.spritesheet('object', 'assets/img/adorable_eevee.jpg', 50, 50);
+              _game.load.spritesheet('object', 'assets/img/Eevee.png', 250, 250);
           },
           create: function(game) {
             _sprite = _game.add.sprite(_game.world.width/2, _game.world.height/2, 'object');
             _sprite.anchor.set(0.5);
-            _sprite.scale.setTo(.5,.5);
+            _sprite.scale.setTo(.25,.25);
 
             _game.physics.enable(_sprite, _game.physics.ARCADE);
 
-            _sprite.body.drag.set(50);
+            //_sprite.body.drag.set(50);
             _sprite.body.maxVelocity.set(200);
             _game.renderer.clearBeforeRender = true;
             _game.renderer.roundPixels = true;
@@ -31,7 +31,8 @@ define(['lib/phaser.min', 'module/Player'],function(Phaser, Player){
             _game.physics.startSystem(_game.physics.ARCADE);
         },
         update: function() {
-
+          _game.physics.arcade.accelerationFromRotation(_sprite.rotation, 200, _sprite.body.acceleration);
+          _sprite.body.angularVelocity = -_objectRotVel;
         }
     }
 });
